0e401be61a0e0704608334efaa1d960e3dc924fe
owinolawrence <owinolawrence21@gmail.com>
2019-11-25 11:02:45 +0300

    create form instance

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,10 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
+from flask import render_template,redirect,url_for
+from .forms import RegistrationForm
+from .. import db
+
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
@@ -19,3 +23,15 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
