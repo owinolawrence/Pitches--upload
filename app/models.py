23743843162b2620ba1caffd5e94bf9a049af28a
owinolawrence <owinolawrence21@gmail.com>
2019-11-25 09:35:43 +0300

    create a pich class and comment class

--- app/models.py
+++ app/models.py
@@ -30,35 +30,37 @@ class User(UserMixin,db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_u, password)
     def __repr__(self):
-    return f'User {self.username}'
-# class Pitch(db.Model):
-#     __tablename__ = 'pitches'
+     return f'User {self.username}'
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
 
-#     id = db.Column(db.Integer, primary_key = True)
-#     title = db.Column(db.String(40))
-#     content = db.Column(db.String)
-#     category = db.Column(db.String(40))
-#     author = db.Column(db.String(40))
-#     upvote = db.Column(db.Integer)
-#     downvote = db.Column(db.Integer)
-#     date_posted = db.Column(db.DateTime, default = datetime.utcnow)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#     comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
-#     def save_pitch(self):
-#         db.session.add(self)
-#         db.session.commit()
-# class Comment(db.Model):
-#         __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(40))
+    content = db.Column(db.String)
+    category = db.Column(db.String(40))
+    author = db.Column(db.String(40))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
+    date_posted = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+class Comment(db.Model):
+    __tablename__ = 'comments'
 
-#         id = db.Column(db.Integer, primary_key = True)
-#         content = db.Column(db.String)
-#         date_posted = db.Column(db.DateTime, default = datetime.utcnow)
-#         user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#         pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
-#      def save_comment(self):
-#         db.session.add(self)
-#         db.session.commit()
-#         @login_manager.user_loader
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String)
+    date_posted = db.Column(db.DateTime, default = datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    @login_manager.user_loader
         
-#     def load_user(user_id):
-#         return User.query.get(int(user_id))
+    def load_user(user_id):
+        return User.query.get(int(user_id))
