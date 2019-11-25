9b9800b29d6765fd998527a67f007e99e0ffff64
owinolawrence <owinolawrence21@gmail.com>
2019-11-25 11:21:42 +0300

    create authenticationroute

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import auth
 from flask import render_template,redirect,url_for, flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
 
@@ -18,3 +18,9 @@ def login():
 
     title = "pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
