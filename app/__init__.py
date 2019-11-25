6c9667c40e712a719d0e508ca1a6e7c44c41e498
owinolawrence <owinolawrence21@gmail.com>
2019-11-25 14:46:15 +0300

    register a  blueprint  and imports

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,14 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_bootstrap import Bootstrap
-from flask_uploads import UploadSet,configure_uploads,IMAGES
-from flask_mail import Mail
+# from flask_uploads import UploadSet,configure_uploads,IMAGES
+# from flask_mail import Mail
 
 app = Flask(__name__)
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-photos = UploadSet('photos',IMAGES)
-mail = Mail()
+# photos = UploadSet('photos',IMAGES)
+# mail = Mail()
 
 
 
@@ -26,7 +26,7 @@ def pitch_app(config_name):
     login_manager.init_app(app)
     db.init_app(app)
     bootstrap.init_app(app)
-    mail.init_app(app)
+    # mail.init_app(app)
     # configure UploadSet
     # configure_uploads(app,photos)
     # registering main blueprint
