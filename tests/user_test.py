706e75c876df4cddeb97745b94fe406fb35b3df5
owinolawrence <owinolawrence21@gmail.com>
2019-11-25 10:26:10 +0300

    create a testcase test_setpassword

--- tests/user_test.py
+++ tests/user_test.py
@@ -7,9 +7,9 @@ class test_userModel(unittest.TestCase):
 def setUp(self):
     self.new_user = User(password='mypassword')
 
-# def test_setPassword(self):
-#     # to test if there is a password
-#     self.assertTrue(self.new_user.secure_pass is not None)
+def test_setPassword(self):
+    # to test if there is a password
+    self.assertTrue(self.new_user.secure_pass is not None)
 
 # def test_check_password(self):
 #     self.assertTrue(self.new_user.check_password('mypassword'))
